0a2da57430928348179de1c5a59d5c54e039ced5
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-04-06 18:05:41 -0300

    Aplicação finalizada

--- api-Rick-Morty/src/App.jsx
+++ api-Rick-Morty/src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
-class App extends React.Component {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ characters: data.results })
+      })
+  }
+
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
-        <p>Consumo API Rick and Monty</p>
+        <h1>
+          Ricky and Morty Personagens:
+        </h1>
+        <div className="body">
+          {characters.map((character) => {
+            return (
+              <div className="container" key={character.name}>
+                <h3>{character.name}</h3>
+                <img src={character.image} alt={character.name} />
+              </div>
+            )
+          })}
+        </div>
       </div>
-    )
+    );
   }
-};
+}
 
-export default App
+export default App;
