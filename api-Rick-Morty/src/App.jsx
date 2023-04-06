9edb5df0e25ed57f385a1e5057cda4d60a02c725
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-04-06 17:41:56 -0300

    Commit inicial, limpeza do arquivo, e transformando App em um componente de classe

---
+++ api-Rick-Morty/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Consumo API Rick and Monty</p>
+      </div>
+    )
+  }
+};
+
+export default App
